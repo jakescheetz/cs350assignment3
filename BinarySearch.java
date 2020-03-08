3287ae85624e865e790e3886e99d287f9ff21e88
Bailey Dauterman <dauterman.bailey@gmail.com>
2020-03-08 16:24:00 -0400

    minor fixes

--- BinarySearch.java
+++ BinarySearch.java
@@ -13,15 +13,18 @@ import java.lang.*;
 
 public class BinarySearchDeluxe {
 
-  /* Returns the index of the first key in a[] that equals the search key, or -1 if no such key. */
+  /* Returns the index of the first key in a[] that equals the search key, or -1 if no such key. 
+   * assumes a sorted array 
+   */
   public  static  <Key>  int  firstIndexOf(Key[]  a,  Key  key,  Comparator<Key> comparator) {
 	  // if any of the arguments passed to firstIndexOf are null then this will throw java.lang.NullPointerException
 	  if (a == null || key == null || comparator == null) throw new java.lang.NullPointerException;
 	  // create instance variables 
-	  int lo = 0, int hi = a.length -1, int mid;
+	  int lo = 0, int hi = a.length - 1, int mid;
 	  // checks to see if the 0th index matches the key
 	  if (comparator.compare(a[lo], key) == 0) { return 0; }
 	  
+	  // loop to find the first match
 	  while (lo <= hi) {
 		  mid = lo + (hi - lo) / 2;
 		  
@@ -44,7 +47,7 @@ public class BinarySearchDeluxe {
 	  // if any of the arguments passed to firstIndexOf are null then this will throw java.lang.NullPointerException
 	  if (a == null || key == null || comparator == null) throw new java.lang.NullPointerException;
 	  // create instance variables 
-	  int lo = 0, int hi = a.length -1, int mid;
+	  int lo = 0, int hi = a.length - 1, int mid;
 	  // checks to see if the last index matches the key
 	  if (comparator.compare(a[hi], key) == 0) { return 0; }
 	  

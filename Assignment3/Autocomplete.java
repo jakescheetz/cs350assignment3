330a0bf4b735beb292feef75e9905b1a4038f325
Ian Iding <Ian Iding@131.238.240.27>
2020-03-08 19:34:28 -0400

    many many changes

---
+++ Assignment3/Autocomplete.java
@@ -0,0 +1,98 @@
+import java.util.Arrays;
+
+// implement sorting algorithm in this class
+public class Autocomplete {
+	
+	Term[] terms;
+	
+
+	// initializes the data structure from the given array of terms.
+	public Autocomplete(Term[] terms) {
+		//creates copy of term array to be sorted
+		this.terms = new Term[terms.length]; 
+		for (int i = 0; i < terms.length; i++) {
+			this.terms[i]=terms[i];
+		}
+		
+		// Lexicographic sort
+		Arrays.sort(this.terms, Term.byPrefixOrder(3));
+		
+	}
+	
+	
+	
+	// returns all terms that start with the given prefix, in descending order by weight.
+	public Term[] allMatches(String prefix) {
+		Term queryTerm = new Term(prefix, 1);
+		
+		
+		// Exception for null prefix
+		if (prefix==null) throw new java.lang.NullPointerException();
+		
+		// Find first and last index of matching terms with query (binary search)
+		int first_match = BinarySearchDeluxe.firstIndexOf(terms, queryTerm, Term.byPrefixOrder(5));
+		// Return if no matches are found
+		if (first_match == -1) return new Term[0];
+		int last_match = BinarySearchDeluxe.lastIndexOf(terms, queryTerm, Term.byPrefixOrder(5));
+		
+		// New array of matching terms
+		Term[] termArray = new Term[last_match-first_match+1];
+		for (int k = first_match; k < termArray.length; k++) {
+			termArray[k] = terms[first_match+k];
+		}
+		
+		// Return list of matching terms in descending order by weight (sort)
+		Arrays.sort(termArray,Term.byReverseWeightOrder());
+		
+		for(int i = 0; i < 20; i++) {
+			System.out.println(termArray[i].query);
+		}
+		
+		return termArray;
+	}
+
+
+
+//public class Merge{ //for running time (n log n)
+//	
+//	//sort for merge sort, recursively 
+//	public static void sort (Comparable[] a, Comparable[] aux, int lo, int hi) {
+//		if (hi <= lo) {
+//			return;
+//		}
+//		int mid = lo + (hi - lo) / 2;
+//		sort(a, aux, lo, mid);
+//		sort(a, aux, mid+1, hi);
+//		merge(a, aux, lo, mid, hi);
+//	}//end sort
+//	
+//	//merge for merge sort, runtime n log n
+//	public static void merge(Comparable[] a, Comparable[] aux, int lo, int mid, int hi) {
+//		for (k = lo; k <= hi; k++) {
+//			aux[k] = a[k];
+//		}
+//		int i = lo, j = mid + 1;
+//		for (int k = lo; k < = hi; k++) {
+//			if (i > mid) {
+//				break;
+//			}
+//			else if (j > hi) {
+//				a[k] = aux[i++];
+//			}
+//			else if (less(aux[j], aux[i])) {
+//				a[k] = a[j++];
+//			}
+//			else {
+//				a[k] = aux[i++]
+//			}
+//		}
+//		
+//	}//end merge method 
+//	
+//	private static boolean less(Comparable v, Comparable w) {
+//		return v.compareTo(w) < 0;
+//	}
+//	
+//}//end merge class 
+
+}

5e8362ba6d8ffc6e1e880a7d55d5b6348989f027
Ian Iding <Ian Iding@131.238.240.27>
2020-03-08 16:23:11 -0400

    steps organized

--- Autocomplete.java
+++ Autocomplete.java
@@ -1,9 +1,30 @@
-
+// implement sorting algorithm in this class
 public class Autocomplete {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+	
+	// initializes the data structure from the given array of terms.
+	public Autocomplete(Term[] terms) {
+		this.terms = terms; 
+		//TODO Sort terms in lexicographic order
+		
+	}
+	
+	
+	
+	// returns all terms that start with the given prefix, in descending order by weight.
+	public Term[] allMatches(String prefix) {
+
+		// Find first and last index of matching terms with query (binary search)
+		
+		// Return list of matching terms in descending order by weight (sort)
+		
+		
+		
+		Term[] termArray;
+		Term[] sortedTerms;
+		
+		return termArray;
 	}
 
 }

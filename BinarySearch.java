f7574608d7bac6f8d5ff45ccc43cbb787d67ea4f
Bailey Dauterman <dauterman.bailey@gmail.com>
2020-03-08 15:15:23 -0400

    update to binary search

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,7 +1,33 @@
+import java.util.*;
+import java.lang.*;
+
+/*
+ * Corner cases. Each static method should throw a java.lang.NullPointerException
+ * if any of its arguments is null. You should assume that the argument array is 
+ * in sorted order (with respect to the supplied comparator).
+ * 
+ * Performance requirements. The firstIndexOf()and lastIndexOf()methods should make
+ * at most 1 + (log2N) compares in the worst case, where Nis the length of the array. 
+ * In this context, a compareis one call to comparator.compare().
+ */
+
 public class BinarySearchDeluxe {
 
   /* Returns the index of the first key in a[] that equals the search key, or -1 if no such key. */
-  public  static  <Key>  int  firstIndexOf(Key[]  a,  Key  key,  Comparator<Key> comparator)
+  public  static  <Key>  int  firstIndexOf(Key[]  a,  Key  key,  Comparator<Key> comparator) {
+	  // if any of the arguments passed to firstIndexOf are null then this will throw java.lang.NullPointerException
+	  if (a == null || key == null || comparator == null) throw new java.lang.NullPointerException;
+	  // create instance variables 
+	  int lo = 0, int hi = a.length -1, int mid;
+	  // 
+	  if (comparator.compare(a[lo], hi) == 0) { return 0; }
+	  
+	  while (lo <= hi) {
+		  mid = lo + (hi - lo) / 2;
+		  
+		  if (comparator.compare(key, a[mid]) < 0)
+	  }
+  }
   
   /* Returns the index of the last key in a[] that equals the search key, or -1 if no such key. */
   public static <Key> int lastIndexOf(Key[] a, Key key, Comparator<Key> comparator)
